befc760faf1e1d2e0a4f9e8ad1fc80105f71530b
Sabeson Kaveendran <sabesonk@codegen.net>
2024-02-16 16:50:17 +0530

    nothing

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask,render_template,request,flash,session
 import sqlite3
 import requests
-from flask_session import Session
+# from flask_session import Session
 
 
 
@@ -17,9 +17,9 @@ login_stat = False
 
 
 app = Flask(__name__)
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
 
 
 	
